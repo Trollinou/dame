1c0257a52aee87bc7f12030e56125d7be5cfd6c3
Etienne Gagnon <etienne.gagnon@gmail.com>
2026-09-11 15:43:41 +0200

    Retrait custom-fields

--- includes/CPT/Contact.php
+++ includes/CPT/Contact.php
@@ -71,7 +71,7 @@ class Contact {
 			'label'               => __( 'Contact', 'dame' ),
 			'description'         => __( 'Contacts externes (Presse, Élus, Clubs voisins)', 'dame' ),
 			'labels'              => $labels,
-			'supports'            => array( 'title', 'custom-fields' ),
+			'supports'            => array( 'title' ),
 			'hierarchical'        => false,
 			'public'              => false,
 			'show_ui'             => true,
